b3fffa08798d6b98d3b0efd7bb3a98dba3549261
CameronDunning <dunningcameron@gmail.com>
2019-09-03 18:25:22 +0000

    Added cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,8 +1,17 @@
+// Imports
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const PORT = 8080;
 
-function generateRandomString() {
+// Activate imports
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Functions
+const generateRandomString = () => {
   let result = "";
   let characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -11,13 +20,9 @@ function generateRandomString() {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
-}
+};
 
-app.set("view engine", "ejs");
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-
+// Data
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -29,21 +34,32 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.post("/urls/:shortURL", (req, res) => {
   let newLongURL = req.body.longURL;
   urlDatabase[req.params.shortURL] = newLongURL;
   res.redirect("/urls");
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-});
-
-app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  console.log(urlDatabase);
-  res.redirect("/urls");
 });
 
 app.post("/urls", (req, res) => {
@@ -53,16 +69,8 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newShortURL}`);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
-  };
-  res.render("urls_show", templateVars);
+app.get("/", (req, res) => {
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
