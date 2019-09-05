c10b2618c4693f9ed995a9a1e52bdc66ed141118
CameronDunning <dunningcameron@gmail.com>
2019-09-05 02:23:54 +0000

    Added cookie session

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 const PORT = 8080;
 const bcrypt = require("bcrypt");
 
@@ -10,6 +11,12 @@ const bcrypt = require("bcrypt");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cookieSession({
+    name: "user_id",
+    keys: ["id"]
+  })
+);
 const {
   urlDatabase,
   users,
@@ -25,13 +32,13 @@ const {
 // Routes
 // Post
 app.post("/login", (req, res) => {
-  // res.cookie("user_id", req.body.user_id);
+  // res.session("user_id", req.body.user_id);
   let notEmpty = checkNotEmptyInput(req);
   let validEmail = checkEmailExists(req);
   if (notEmpty && validEmail) {
     if (validLogin(req)) {
       let user = getUserObj(req.body.email);
-      res.cookie("user_id", user);
+      req.session.user_id = user;
       res.redirect("/urls");
     } else {
       res.status(403).send("Invalid Login");
@@ -52,8 +59,8 @@ app.post("/register", (req, res) => {
   let notEmpty = checkNotEmptyInput(req);
   let validEmail = !checkEmailExists(req);
   if (notEmpty && validEmail) {
-    user_id = addNewUser(req);
-    res.cookie("user_id", user_id);
+    user = addNewUser(req);
+    req.session.user_id = user;
     res.redirect("/urls");
   } else if (!validEmail) {
     res.status(400).send("Email Already Exists");
@@ -63,10 +70,10 @@ app.post("/register", (req, res) => {
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  if (!req.cookies.user_id) {
+  if (!req.session.user_id) {
     res.redirect("/login");
   } else {
-    let user = req.cookies.user_id.id;
+    let user = req.session.user_id.id;
     if (user === urlDatabase[req.params.shortURL].userID) {
       delete urlDatabase[req.params.shortURL];
       res.redirect("/urls");
@@ -77,10 +84,10 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.post("/urls/:shortURL", (req, res) => {
-  if (!req.cookies.user_id) {
+  if (!req.session.user_id) {
     res.redirect("/login");
   } else {
-    let user = req.cookies.user_id.id;
+    let user = req.session.user_id.id;
     if (user === urlDatabase[req.params.shortURL].userID) {
       let newLongURL = req.body.longURL;
       urlDatabase[req.params.shortURL].longURL = newLongURL;
@@ -96,22 +103,22 @@ app.post("/urls", (req, res) => {
   let newLongURL = req.body.longURL;
   urlDatabase[newShortURL] = {};
   urlDatabase[newShortURL].longURL = newLongURL;
-  urlDatabase[newShortURL].userID = req.cookies.user_id.id;
+  urlDatabase[newShortURL].userID = req.session.user_id.id;
   res.redirect(`/urls/${newShortURL}`);
 });
 
 // Get
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
+  const longURL = urlDatabase[shortURL].longURL;
   res.redirect(longURL);
 });
 
 app.get("/urls/new", (req, res) => {
   let templateVars = {
-    user_id: req.cookies["user_id"]
+    user_id: req.session["user_id"]
   };
-  if (req.cookies.user_id) {
+  if (req.session.user_id) {
     res.render("urls_new", templateVars);
   } else {
     res.redirect("/login");
@@ -119,15 +126,15 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  if (!req.cookies.user_id) {
+  if (!req.session.user_id) {
     res.redirect("/login");
   } else {
-    let user = req.cookies.user_id.id;
+    let user = req.session.user_id.id;
     if (user === urlDatabase[req.params.shortURL].userID) {
       let templateVars = {
         shortURL: req.params.shortURL,
         longURL: urlDatabase[req.params.shortURL].longURL,
-        user_id: req.cookies["user_id"]
+        user_id: req.session["user_id"]
       };
       res.render("urls_show", templateVars);
     } else {
@@ -148,12 +155,12 @@ app.get("/login", (req, res) => {
 
 app.get("/urls", (req, res) => {
   let userURLs = {};
-  if (req.cookies["user_id"]) {
-    userURLs = urlsForUser(req.cookies["user_id"].id);
+  if (req.session["user_id"]) {
+    userURLs = urlsForUser(req.session["user_id"].id);
   }
   let templateVars = {
     urls: userURLs,
-    user_id: req.cookies["user_id"]
+    user_id: req.session["user_id"]
   };
   res.render("urls_index", templateVars);
 });
